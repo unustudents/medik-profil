c101b5ca753e974d26a2edc0ed0f7f86ee020588
Mas Akmal <nandaakmal40@gmail.com>
2026-01-28 00:39:22 +0700

    Dashboard

--- src/consts.ts
+++ src/consts.ts
@@ -5,3 +5,20 @@ export const SITE_TITLE = 'RSU Afdila';
 export const SITE_DESCRIPTION = 'Rumah Sakit Umum Afdila Cilacap';
 export const SITE_URL = 'https://rsuafdila.com';
 export const COPYRIGHT = 'IT Department RSU Afdila';
+
+export const ADDRESS = 'Jln. Soekarno Hatta No.79 - Rejasari, Cilacap';
+export const PHONE_EMERGENCY = '+62 000 111 22';
+export const PHONE_CONTACT = '081532744804';
+export const EMAIL_CONTACT = 'rsuafdila@gmail.com';
+
+export const OPENING_HOURS = {
+    WEEKDAY: '08:00 - 22:00',
+    WEEKEND: '08:00 - 22:00'
+};
+
+export const SOCIALS = [
+    { name: 'Facebook', icon: 'f', url: '#' },
+    { name: 'X', icon: '𝕏', url: '#' },
+    { name: 'Instagram', icon: 'ig', url: '#' },
+    { name: 'Youtube', icon: 'yt', url: '#' }
+];
