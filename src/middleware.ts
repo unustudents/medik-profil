5244bda45db0a065512fd25708d5aa6855be167e
Head IT RS Afdila <it.rsuafdila@gmail.com>
2026-02-21 11:41:23 +0700

    chore: Update Content Security Policy to allow Cloudflare insights and remove the search bar from the header.

--- src/middleware.ts
+++ src/middleware.ts
@@ -44,7 +44,7 @@ export const onRequest = defineMiddleware(async (_context, next) => {
         "Content-Security-Policy",
         [
             "default-src 'self'",
-            "script-src 'self' 'unsafe-inline'",
+            "script-src 'self' 'unsafe-inline' https://static.cloudflareinsights.com",
             "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
             "font-src 'self' https://fonts.gstatic.com",
             "img-src 'self' data: https: http:",
